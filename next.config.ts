89ecdd513128a94c9f67198ea1f1b03e2a1c5cc7
Md Abdul Ahad Linkon <m.alinkon10@gmail.com>
2026-05-07 16:08:25 +0600

    feat:port fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const backendApiUrl = process.env.API_BASE_URL ?? "http://localhost:3001/api/v1";
+const backendApiUrl = process.env.API_BASE_URL ?? "http://localhost:5010/api/v1";
 
 const nextConfig: NextConfig = {
   async rewrites() {
