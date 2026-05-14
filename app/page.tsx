fa7305dd27a1193f80418490904a46be4035ef88
Shazzad_softzino <shazzad@softzino.com>
2026-05-14 12:16:44 +0600

    Header Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Header from '@/components/sections/header';
 import TopHeader from '@/components/sections/top-header';
 
 export default function Home() {
   return (
     <div>
       <TopHeader />
+      <Header />
     </div>
   );
 }
