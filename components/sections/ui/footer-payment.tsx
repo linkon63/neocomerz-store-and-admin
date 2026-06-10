65234caa13938bba1d5ad8c16fbf55b33c1254f3
Shazzad_softzino <shazzad@softzino.com>
2026-06-10 10:17:18 +0600

    update Footer

--- components/sections/ui/footer-payment.tsx
+++ components/sections/ui/footer-payment.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function FooterPayment() {
   return (
     <div className="text-center md:text-left">
-      <span className="font-['Gotham'] text-brand-6 text-sm font-medium block mb-2">We Using Safe Payment For</span>
+      <span className="font-['Gotham'] text-white text-sm font-medium block mb-2">We Using Safe Payment For</span>
       <div className="flex items-center gap-2 justify-center md:justify-start flex-wrap">
         <Image src="/images/payment/Bkash.svg" alt="Bkash" width={30} height={20} className="object-contain" />
         <Image src="/images/payment/Nagad.svg" alt="Nagad" width={30} height={20} className="object-contain" />
