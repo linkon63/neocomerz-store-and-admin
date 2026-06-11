2b0bafe638910639e48cabed1d47fe5675623a4c
Shazzad_softzino <shazzad@softzino.com>
2026-06-11 11:05:56 +0600

    About us in Progress

--- components/sections/about-us/about.tsx
+++ components/sections/about-us/about.tsx
@@ -1,10 +1,12 @@
 import Brands from '@/components/sections/brands';
 import Instagram from '@/components/sections/instagram';
 import Sustainable from '@/components/sections/sustainable';
+import Quality from '@/components/sections/quality';
 
 export default function About() {
   return (
     <div>
+      <Quality />
       <Sustainable />
       <Brands />
       <Instagram />
---
+++ components/sections/quality.tsx
@@ -0,0 +1,12 @@
+
+export default function Quality() {
+  return (
+    <section className="bg-white w-full">
+      <div className="quality-wrapper"> 
+        <div className="container mx-auto py-3">
+            
+        </div>
+      </div>
+    </section>
+  );
+}
