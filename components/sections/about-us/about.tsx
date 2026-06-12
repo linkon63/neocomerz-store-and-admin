f553ad17793d18337a33d59d4f993cd9f2d31248
Shazzad_softzino <shazzad@softzino.com>
2026-06-12 09:53:59 +0600

    about in progress

--- components/sections/about-us/about.tsx
+++ components/sections/about-us/about.tsx
@@ -2,10 +2,12 @@ import Brands from '@/components/sections/brands';
 import Instagram from '@/components/sections/instagram';
 import Sustainable from '@/components/sections/sustainable';
 import Quality from '@/components/sections/quality';
+import TheBrands from '@/components/sections/ui/the-brands';
 
 export default function About() {
   return (
     <div>
+      <TheBrands />
       <Quality />
       <Sustainable />
       <Brands />
